fd91beae8269887e76e95851d41efa17c3578056
monx.voll <stedy3348112688@gmail.com>
2024-10-10 20:00:08 -0500

    Se añade un menu de opciones basico

---
+++ RiddleSistem/src/main/java/view/Main.java
@@ -0,0 +1,33 @@
+package view;
+
+import model.BinaryTree;
+import model.ManagementPersistence;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        ManagementPersistence persistence = new ManagementPersistence();
+        Scanner scanner = new Scanner(System.in);
+        // Se carga el arbol desde el archivo JSON
+        BinaryTree tree = persistence.loadTree();
+
+        System.out.println("Por favor digite una opcion : ");
+        System.out.println("1.Iniciar juego");
+        System.out.println("2.Ver arbol");
+        int option = scanner.nextInt();
+        scanner.nextLine(); // evita que se cole un salto d linea
+        switch (option){
+            case 1:
+                tree.changeSon();
+                // Se guarda el arbol en el archivo JSON
+                persistence.saveTree(tree); // metodo que servira en un futuro cuando se añada la funcion de añadir preguntas nuevas
+                break;
+            case 2:
+                tree.printTree();
+                break;
+            default:
+                System.out.println("digite una opcion valida");
+        }
+    }
+}
