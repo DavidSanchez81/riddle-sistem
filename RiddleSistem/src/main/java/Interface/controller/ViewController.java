a6d77d6112036026df4f1ed5fd7913226e174a79
monx.voll <stedy3348112688@gmail.com>
2024-10-16 21:43:00 -0500

    se enlaza con el listener

--- RiddleSistem/src/main/java/Interface/controller/ViewController.java
+++ RiddleSistem/src/main/java/Interface/controller/ViewController.java
@@ -1,14 +1,17 @@
-package Interface.listeners;
+package Interface.controller;
 
 
+import Interface.controller.listeners.Test;
 import Interface.panels.MainPanel;
 
 public class ViewController {
    private MainPanel mainPanel;
+   private Test test;
 
 
    public ViewController(){
       this.mainPanel = new MainPanel();
+      this.test = new Test(this);
    }
 
    //Getter
